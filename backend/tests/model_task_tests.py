669e7503f66f3ddcf99cf926bc73a466afcccf46
Richard Ma <richard.ma.19850509@gmail.com>
2018-01-09 22:59:48 +0800

    add task unit testing

--- backend/tests/model_task_tests.py
+++ backend/tests/model_task_tests.py
@@ -14,5 +14,31 @@ class ModelTaskTestCase(TestCase):
     def tearDown(self):
         db.drop_all()
 
-    def test_task(self):
-        return True
+    def test_task_CRUD(self):
+        origin_task = Task()
+        origin_task.name = 'origin task'
+        origin_task.status = False
+
+        # create
+        db.session.add(origin_task)
+        db.session.commit()
+
+        # read
+        tasks = Task.query.filter_by(name=origin_task.name)
+        task = tasks.first()
+        self.assertEqual(1, tasks.count())
+        self.assertEqual(origin_task.status, task.status)
+
+        # update
+        task.status = True
+        db.session.commit()
+        tasks = Task.query.filter_by(name=origin_task.name)
+        task = tasks.first()
+        self.assertEqual(True, task.status)
+
+        # delete
+        db.session.delete(task)
+        db.session.commit()
+
+        tasks = Task.query.filter_by(name=task.name)
+        self.assertEqual(0, tasks.count())
