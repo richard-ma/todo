b5199c268a2a979696991f9ed556a11d77d18c94
Richard Ma <richard.ma.19850509@gmail.com>
2018-01-15 15:04:51 +0800

    add frontend basic ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+
+function TaskBadge(props) {
+  if (props.status) {
+    return (<span className="badge badge-warning">Done</span>);
+  } else {
+    return (<span className="badge badge-success">Processing</span>);
+  }
+}
+
+class Task extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      id: this.props.id,
+      name: this.props.name,
+      status: this.props.status,
+    };
+  }
+
+  handleClick(e) {
+    // TODO Update API
+    this.setState((prevState) => ({
+      status: !prevState.status
+    }));
+  }
+
+  render() {
+    return (
+      <li className="list-group-item" onClick={this.handleClick}>
+        <TaskBadge status={this.state.status}/>
+        <span className="form-check-label">{this.state.name}</span>
+      </li>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ul className="list-group">
+        <Task name='hello' status={true} />
+        <Task name='hi' status={false} />
+      </ul>
     );
   }
 }
